5c549c37a7a1f22f8a372e7765fe406a1090be86
CovChEGG <covchegg@gmail.com>
2022-03-08 15:14:20 +0700

    corected function name

--- Seminars/08_WhatQuarterIs.py
+++ Seminars/08_WhatQuarterIs.py
@@ -1,14 +1,14 @@
 # 08. Сообщить в какой четверти координатной плоскости
 # или на какой оси находится точка с координатами Х и У
-def input_Int(txt):
+def input_int(txt):
     while True:
         in_string=input('Введите '+ txt + ': ')
         if in_string.isdigit() == 1:
             return int(in_string)
         print('\n!!! Не корректный ввод!!! Попробуйте ещё, пожалуйста.\n')
 
-X = input_Int("X")
-Y = input_Int("Y")
+X = input_int("X")
+Y = input_int("Y")
 print(X,Y)
 if X == 0 and Y == 0:
     print("Точка находится в начале координат")
