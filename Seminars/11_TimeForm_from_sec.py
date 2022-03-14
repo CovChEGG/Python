7a3c91b7cf0407dd6a71bf35fcebd8a0f9f3dc9e
CovChEGG <CovChEGG@gmail.com>
2022-03-14 22:08:09 +0700

    reformatting code

--- Seminars/11_TimeForm_from_sec.py
+++ Seminars/11_TimeForm_from_sec.py
@@ -1,7 +1,7 @@
 # Сделайте так, чтобы число секунд отображалось в виде дни:часы:минуты:секунды.
 def int_input(txt):
     while True:
-        in_string=input('Введите '+ txt + ': ')
+        in_string = input('Введите ' + txt + ': ')
         if in_string.isdigit() == 1:
             in_int = int(in_string)
             if in_int > 0:
@@ -10,9 +10,9 @@ def int_input(txt):
 
 
 S = int_input('количество секунд')
-time_format="{0}:{1}:{2}:{3}".format(S//86400,
-                                     (S%86400)//3600,
-                                     ((S%86400)%3600)//60,
-                                     ((S%86400)%1440)%60
-                                    )
+time_format = "{0}:{1}:{2}:{3}".format(S // 86400,
+                                       (S % 86400) // 3600,
+                                       ((S % 86400) % 3600) // 60,
+                                       ((S % 86400) % 1440) % 60
+                                       )
 print(time_format)
