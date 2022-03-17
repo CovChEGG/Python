0f4418c05172def96e6ed0295228be0ee7f4e7be
CovChEGG <CovChEGG@gmail.com>
2022-03-18 00:38:16 +0700

    task 18 - num of occurrence with method .count is added

---
+++ Seminars/18_13_Elem_of_One_String_to_another.py
@@ -0,0 +1,13 @@
+# Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+string_one = input("Введите первую строку: ")
+string_two = input("Введите вторую строку: ")
+count_in_one = string_one.count(string_two)
+count_in_two = string_two.count(string_one)
+if count_in_one is count_in_two:
+    print("Строки идентичны, количество вхождений равно: 1")
+elif count_in_one:
+    print(f"Количество вхождений второй строки в первую равно: {count_in_one}")
+elif count_in_two:
+    print(f"Количество вхождений первой строки во вторую равно: {count_in_two}")
+else:
+    print("Нет вхождений одной строки в другую! Количество вхождений равно: 0")
