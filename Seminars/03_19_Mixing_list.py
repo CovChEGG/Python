8728c81d65742183dcb3eef32b44aa5be2fda75a
CovChEGG <CovChEGG@gmail.com>
2022-04-02 00:59:51 +0700

    added 03_19 Mixing list with (randint and) random.shuffle() functions

--- Seminars/03_19_Mixing_list.py
+++ Seminars/03_19_Mixing_list.py
@@ -1,11 +1,16 @@
 # Реализовать алгоритм перемешивания списка.
 from random import randint
 
+import random
+
 import my_funcs as mf
 
 n = mf.int_input_n('натуральное число N', 0)
 input_list = [i for i in range(-n, n + 1)]
+shuffle_list = [i for i in range(-n, n + 1)]
+random.shuffle(shuffle_list)
 print(input_list)
+print(shuffle_list)
 output_list = []
 tmp = len(input_list)-1
 for i in range(len(input_list)):
